dd4cfd01172021552fdf46b442fb6be7b8dacaca
Kevin McBride <kevin.w.mcbride.86@gmail.com>
2021-04-30 13:00:05 -0400

    updated some syntax

--- examples/example_2/Ex_2_estimation.py
+++ examples/example_2/Ex_2_estimation.py
@@ -15,25 +15,39 @@ if __name__ == "__main__":
         if int(sys.argv[1]):
             with_plots = False
  
+    
+    # You need to solve a smaller model
+    # Pass the parameters to the new model
+    # Re-optimize the second model
+    
     kipet_model = KipetModel()
     
     r1 = kipet_model.new_reaction('reaction-1')
 
     # Add the model parameters
-    k1 = r1.parameter('k1', value=2, bounds=(0.0, 5.0))
-    k2 = r1.parameter('k2', value=0.2, bounds=(0.0, 2.0))
+    k1 = r1.parameter('k1', value=0.3, bounds=(0.0, 5.0))
+    k2 = r1.parameter('k2', value=1.4, bounds=(0.0, 3.0))
     
     # Declare the components and give the initial values
     A = r1.component('A', value=1)
     B = r1.component('B', value=0.0)
     C = r1.component('C', value=0.0)
     
+    
+    file_name = '/home/kevin/Dev/kipet_examples/examples/example_2/data/Dij.txt'
+    full_data = kipet_model.read_data_file(file_name)
+
+    wl_reduction = 1
+    ts_reduction = 1
+
+    small_data = full_data.loc[::ts_reduction, ::wl_reduction]
+    
     # Use this function to replace the old filename set-up
-    r1.add_data(category='spectral', file='data/Dij.txt')
+    r1.add_data(category='spectral', data=small_data)
     
     # Preprocessing!
     #r1.spectra.msc()
-    r1.spectra.decrease_wavelengths(3)
+    r1.spectra.decrease_wavelengths(1)
 
     # define explicit system of ODEs
     rates = {}
@@ -49,16 +63,14 @@ if __name__ == "__main__":
     r1.settings.collocation.ncp = 1
     r1.settings.collocation.nfe = 60
     r1.settings.parameter_estimator.tee = True
-    r1.settings.parameter_estimator.solver = 'ipopt_sens'
-    r1.settings.general.initialize_pe = True
-    r1.settings.general.scale_pe = True
+    r1.settings.parameter_estimator.solver = 'ipopt'
+    #r1.settings.general.initialize_pe = True
+    #r1.settings.general.scale_pe = True
+    r1.settings.parameter_estimator.sim_init = False
+        
     
-    # This is all you need to run KIPET!
     r1.run_opt()
+    r1.plot('Z')
     
-    # Display the results
-    r1.results.show_parameters
     
-    # Plot results    
-    if with_plots:
-        r1.plot()
+ 
--- examples/example_6/Ex_6_non_absorbing.py
+++ examples/example_6/Ex_6_non_absorbing.py
@@ -20,38 +20,54 @@ if __name__ == "__main__":
     r1 = kipet_model.new_reaction('reaction-1')
     
     # Add the model parameters
-    k1 = r1.parameter('k1', value=2, bounds=(0.0, 5.0))
-    k2 = r1.parameter('k2', value=0.2, bounds=(0.0, 2.0))
+    k1 = r1.parameter('k1', value=1.5, bounds=(0.01, 4.0))
+    k2 = r1.parameter('k2', value=0.33, bounds=(0.01, 2.0))
     
     # Declare the components and give the initial values
-    A = r1.component('A', value=7.5e-5)
-    B = r1.component('B', value=0.0)
+    # Perhaps use an Enum on components...
+    A = r1.component('A', value=1)
+    B = r1.component('B', value=0.0, known=True, bounds=(1e-8, 1e-4))
     C = r1.component('C', value=0.0, absorbing=False)
     
     # Use this function to replace the old filename set-up
-    r1.add_data(category='spectral', file='data/Dij.txt')
+    #r1.add_data(category='spectral', file='/home/kevin/Dev/kipet_examples/examples/example_6/data/Dij.txt')    
     
-    # define explicit system of ODEs
-    rA = k1*A
-    rB = k2*B
+    file = '/home/kevin/Dev/kipet_examples/examples/example_6/data/non_abs.csv'
+    full_data = kipet_model.read_data_file(file)
+    r1.add_data(category='spectral', data=full_data)
+    
+    #% Try to make stoic matrix from given inputs
+    
+    rA = r1.add_reaction('rA', k1*A, description='Reaction A' )
+    rB = r1.add_reaction('rB', k2*B, description='Reaction B' )
     
     r1.add_ode('A', -rA)
     r1.add_ode('B', rA - rB)
     r1.add_ode('C', rB)
 
+    # Use the stoichiometry to build the reaction network:
+    stoich_data = {'rA': [-1, 1, 0],
+                   'rB': [0, -1, 1]}
+    
+    r1.build_from_reaction_matrix(stoich_data)
+    
     # Settings
-    r1.settings.collocation.ncp = 1
+    r1.settings.collocation.ncp = 3
     r1.settings.collocation.nfe = 60
     r1.settings.variance_estimator.use_subset_lambdas = True
     r1.settings.variance_estimator.max_iter = 5
     r1.settings.variance_estimator.tolerance = 1e-4
     r1.settings.parameter_estimator.tee = False
     
+    #r1.settings.general.initialize_pe = True
+    #r1.settings.general.scale_pe = True
+    r1.settings.parameter_estimator.sim_init = True
+    
     r1.run_opt()
     
     # Display the results
     r1.results.show_parameters
     
-        # New plotting methods
+    # New plotting methods
     if with_plots:
         r1.plot()
