13f81c6a9f3778f5c9b6b0028872e02065732696
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-22 15:33:47 +0200

    Updated Ex 2

--- examples/example_2/Ex_2_estimation.py
+++ examples/example_2/Ex_2_estimation.py
@@ -1,28 +1,19 @@
-"""Example 2: Estimation with new KipetModel"""
-
+"""
+Example 2: Simple example showing parameter fitting using spectral data
+"""
 # Standard library imports
 import sys # Only needed for running the example from the command line
 
-# Third party imports
-
 # Kipet library imports
-from kipet import KipetModel
+import kipet
 
 if __name__ == "__main__":
 
     with_plots = True
-    if len(sys.argv)==2:
-        if int(sys.argv[1]):
-            with_plots = False
- 
+    if len(sys.argv)==2 and int(sys.argv[1]):
+        with_plots = False
     
-    # You need to solve a smaller model
-    # Pass the parameters to the new model
-    # Re-optimize the second model
-    
-    kipet_model = KipetModel()
-    
-    r1 = kipet_model.new_reaction('reaction-1')
+    r1 = kipet.ReactionModel('reaction-1')
 
     # Add the model parameters
     k1 = r1.parameter('k1', value=0.3, bounds=(0.0, 5.0))
@@ -33,44 +24,30 @@ if __name__ == "__main__":
     B = r1.component('B', value=0.0)
     C = r1.component('C', value=0.0)
     
-    
-    file_name = '/home/kevin/Dev/kipet_examples/examples/example_2/data/Dij.txt'
-    full_data = kipet_model.read_data_file(file_name)
+    # Input data
+    file_name = 'data/Dij.txt'
+    r1.add_data(category='spectral', file=file_name)
 
-    wl_reduction = 1
-    ts_reduction = 1
-
-    small_data = full_data.loc[::ts_reduction, ::wl_reduction]
+    # Input the reactions as expressions
+    rA = r1.add_reaction('rA', k1*A)
+    rB = r1.add_reaction('rB', k2*B)
     
-    # Use this function to replace the old filename set-up
-    r1.add_data(category='spectral', data=small_data)
-    
-    # Preprocessing!
-    #r1.spectra.msc()
-    r1.spectra.decrease_wavelengths(1)
-
-    # define explicit system of ODEs
-    rates = {}
-    rates['A'] = -k1 * A
-    rates['B'] = k1 * A - k2 * B
-    rates['C'] = k2 * B
-    
-    r1.add_odes(rates)
+    # Input the ODEs
+    r1.add_ode('A', -rA )
+    r1.add_ode('B', rA - rB )
+    r1.add_ode('C', rB )
     
     r1.bound_profile(var='S', bounds=(0, 10))
 
-    # Settings
+    # Change some of the default settings
     r1.settings.collocation.ncp = 1
     r1.settings.collocation.nfe = 60
     r1.settings.parameter_estimator.tee = True
-    r1.settings.parameter_estimator.solver = 'ipopt'
-    #r1.settings.general.initialize_pe = True
-    #r1.settings.general.scale_pe = True
-    r1.settings.parameter_estimator.sim_init = False
-        
     
+    # Parameter fitting
     r1.run_opt()
-    r1.plot('Z')
     
+    # Create plots
+    if with_plots:
+        r1.plot()
     
- 
