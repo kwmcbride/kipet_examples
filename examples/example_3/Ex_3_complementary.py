9294cdc25cfdb6927b8ba61018a9ee7d29c382ff
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-22 15:43:41 +0200

    Updated Ex 3

--- examples/example_3/Ex_3_complementary.py
+++ examples/example_3/Ex_3_complementary.py
@@ -1,5 +1,8 @@
-"""Example 3: Simulation using complementatry states with new KipetModel"""
+"""
+Example 3: Simulation example using complementary states
 
+Note: When using functions such as exp, you need to import them from pyomo.core
+"""
 # Standard library imports
 import sys # Only needed for running the example from the command line
 
@@ -7,18 +10,16 @@ import sys # Only needed for running the example from the command line
 from pyomo.core import exp
 
 # Kipet library imports
-from kipet import KipetModel
+import kipet
+
 
 if __name__ == "__main__":
 
     with_plots = True
-    if len(sys.argv)==2:
-        if int(sys.argv[1]):
-            with_plots = False
-
-    kipet_model = KipetModel()
+    if len(sys.argv)==2 and int(sys.argv[1]):
+        with_plots = False
     
-    r1 = kipet_model.new_reaction('reaction-1')
+    r1 = kipet.ReactionModel('reaction-1')
     
     # Declare the components and give the initial values
     A = r1.component('A', value=1.0)
@@ -28,12 +29,14 @@ if __name__ == "__main__":
     T = r1.state('T', value=290, description='Temperature')
     V = r1.state('V', value=100, description='Volumne')
 
-    # Define the ODEs
+    # Define the expressions - note that expression method is not used!
     k1 = 1.25*exp((9500/1.987)*(1/320.0 - 1/T))
     k2 = 0.08*exp((7000/1.987)*(1/290.0 - 1/T))
+    
     ra = -k1*A
     rb = 0.5*k1*A - k2*B
     rc = 3*k2*B
+    
     cao = 4.0
     vo = 240
     T1 = 35000*(298 - T)
@@ -41,18 +44,23 @@ if __name__ == "__main__":
     T3 = V*(6500.0*k1*A - 8000.0*k2*B)
     Den = (30*A + 60*B + 20*C)*V + 3500.0
     
+    # Add ODEs
     r1.add_ode('A', ra + (cao - A)/V )
     r1.add_ode('B', rb - B*vo/V )
     r1.add_ode('C', rc - C*vo/V )
     r1.add_ode('T', (T1 + T2 + T3)/Den )
     r1.add_ode('V', vo )
     
-    r1.set_times(0.0, 2.0)
+    # Simulation requires a time span
+    r1.set_time(2.0)
     
+    # Change some of the default settings
     r1.settings.collocation.nfe = 20
     r1.settings.collocation.ncp = 1
 
+    # Simulation
     r1.simulate()  
 
+    # Create plots
     if with_plots:
         r1.plot()
