b4a14aedeaa16e09d351e300fd5a06374a2f91ef
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-24 17:50:03 +0200

    Updated to ReactionLab

--- examples/example_12/Ex_12_multiple_experiments_concentration.py
+++ examples/example_12/Ex_12_multiple_experiments_concentration.py
@@ -9,19 +9,19 @@ import sys # Only needed for running the example from the command line
 # Third party imports
 
 # Kipet library imports
-from kipet import KipetModel
+import kipet
+
 
 if __name__ == "__main__":
 
     with_plots = True
-    if len(sys.argv)==2:
-        if int(sys.argv[1]):
-            with_plots = False
+    if len(sys.argv)==2 and int(sys.argv[1]):
+        with_plots = False
  
     # Define the general model
-    kipet_model = KipetModel()
+    lab = kipet.ReactionLab()
     
-    r1 = kipet_model.new_reaction(name='reaction-1')
+    r1 = lab.new_reaction(name='reaction-1')
     
     # Add the parameters
     k1 = r1.parameter('k1', value=1.0, bounds=(0.0, 10.0))
@@ -33,12 +33,13 @@ if __name__ == "__main__":
     C = r1.component('C', value=0.0)
     
     # define explicit system of ODEs
-    rates = {}
-    rates['A'] = -k1 * A
-    rates['B'] = k1 * A - k2 * B
-    rates['C'] = k2 * B
+    rA = k1*A
+    rB = k2*B
     
-    r1.add_odes(rates)
+    # Define the reaction model
+    r1.add_ode('A', -rA )
+    r1.add_ode('B', rA - rB )
+    r1.add_ode('C', rB )
    
     # Add the dataset for the first model
     r1.add_data('C_data', file='data/Ex_1_C_data.txt')
@@ -46,19 +47,17 @@ if __name__ == "__main__":
     # Add the known variances
     r1.variances = {'A':1e-10,'B':1e-10,'C':1e-10}
     
-    r2 = kipet_model.new_reaction(name='reaction-2', model=r1)
+    r2 = lab.new_reaction(name='reaction-2', model=r1)
    
     # Add the dataset for the first model
-    noised_data = kipet_model.add_noise_to_data(r1.data['C_data'], 0.0001) 
+    noised_data = kipet.add_noise_to_data(r1.datasets['C_data'].data, 0.0001) 
     r2.add_data('C_data', data=noised_data[::10])
     
     # Add the known variances
     r2.components.update('variance', {'A':1e-4,'B':1e-4,'C':1e-4})
     # # Create the MultipleExperimentsEstimator and perform the parameter fitting
-    kipet_model.run_opt()
+    lab.run_opt()
 
     # Plot the results
-    if with_plots:     
-        for name, model in kipet_model.models.items():
-            kipet_model.results[name].show_parameters
-            model.plot()
+    lab.show_parameters
+    lab.plot('Z')
