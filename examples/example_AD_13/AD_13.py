c809573ffd44266cdd7ceb4160acf5357e55e47c
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-24 18:10:54 +0200

    Updated AD_13

--- examples/example_AD_13/AD_13.py
+++ examples/example_AD_13/AD_13.py
@@ -7,18 +7,18 @@ import sys
 # Third party imports
 
 # KIPET library imports
-from kipet import KipetModel
+import kipet
+
 
 if __name__ == "__main__":
 
     with_plots = True
-    if len(sys.argv)==2:
-        if int(sys.argv[1]):
-            with_plots = False
+    if len(sys.argv)==2 and int(sys.argv[1]):
+        with_plots = False
     
-    kipet_model = KipetModel()
+    lab = kipet.ReactionLab()
  
-    r1 = kipet_model.new_reaction('reaction-1')   
+    r1 = lab.new_reaction('reaction-1')   
  
     # Add the model parameters
     k1 = r1.parameter('k1', value=1.0, bounds=(0.0, 10.0), fixed=False)
@@ -34,30 +34,27 @@ if __name__ == "__main__":
     
     r1.add_data(file=filename)
     
-    rates = {}
-    rates['A'] = -k1 * A
-    rates['B'] = k1 * A - k2 * B
-    rates['C'] = k2 * B
+    # Define the reaction model
+    r1.add_ode('A', -k1 * A )
+    r1.add_ode('B', k1 * A - k2 * B )
+    r1.add_ode('C', k2 * B )
     
-    r1.add_odes(rates)
-
     # Repeat for the second model - the only difference is the dataset    
-    r2 = kipet_model.new_reaction(name='reaction-2', model=r1)
+    r2 = lab.new_reaction(name='reaction-2', model=r1)
     # Simulated second dataset with noise
-    noised_data = kipet_model.add_noise_to_data(r1.datasets['ds1'].data, 0.0001) 
+    noised_data = kipet.add_noise_to_data(r1.datasets['ds-1'].data, 0.0001) 
     
     # Add the dataset for the second model
     r2.add_data(data=noised_data)
 
-    kipet_model.settings.solver.solver = 'ipopt_sens'
+    lab.settings.solver.solver = 'ipopt_sens'
 
     r1.variances = {'device':1e-10,'A':1e-10,'B':1e-10,'C':1e-10}
     r2.variances = {'device':1e-4,'A':1e-4,'B':1e-4,'C':1e-4}
     
-    kipet_model.run_opt()
+    lab.run_opt()
     
     # Plot the results
     if with_plots:
-        for name, model in kipet_model.models.items():
-            kipet_model.results[name].show_parameters
-            model.plot()
+        lab.show_parameters
+        lab.plot()
