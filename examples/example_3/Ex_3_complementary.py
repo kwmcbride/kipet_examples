168f543e90a3db1a626765b493db70b76caf1981
kwmcbride <kevin.w.mcbride.86@gmail.com>
2021-05-25 20:21:10 +0200

    Updated syntax

--- examples/example_3/Ex_3_complementary.py
+++ examples/example_3/Ex_3_complementary.py
@@ -29,40 +29,61 @@ if __name__ == "__main__":
     T = r1.state('T', value=290, description='Temperature')
     V = r1.volume(value=100, description='Volume')
 
-    # Define the expressions - note that expression method is not used!
-    k1 = 1.25*exp((9500/1.987)*(1/320.0 - 1/T))
-    k2 = 0.08*exp((7000/1.987)*(1/290.0 - 1/T))
+    # Model constants
+    cA0 = r1.constant('ca0', value=4.0)
+    v0 = r1.constant('v0', value=240)
     
-    ra = -k1*A
-    rb = 0.5*k1*A - k2*B
-    rc = 3*k2*B
+    cpA = r1.constant('cpA', value=30)
+    cpB = r1.constant('cpB', value=60)
+    cpC = r1.constant('cpC', value=20)
     
-    cao = 4.0
-    vo = 240
-    T1 = 35000*(298 - T)
-    T2 = 4*240*30.0*(T-305.0)
-    T3 = V*(6500.0*k1*A - 8000.0*k2*B)
-    Den = (30*A + 60*B + 20*C)*V + 3500.0
+    TA0 = r1.constant('TA0', value=305)
+    hrxnA = r1.constant('hrxnA', value=6500)
+    hrxnB = r1.constant('hrxnB', value=8000)
+    Tc = r1.constant('Tc', value=298)
+    Uc = r1.constant('Uc', value=35000)
+
+    # Define the expressions
+    k1 = r1.add_expression('k1', 1.25*exp((9500/1.987)*(1/320.0 - 1/T)))
+    k2 = r1.add_expression('k2', 0.08*exp((7000/1.987)*(1/290.0 - 1/T)))
     
-    # Add ODEs
-    r1.add_ode('A', ra + (cao - A)/V )
-    r1.add_ode('B', rb - B*vo/V )
-    r1.add_ode('C', rc - C*vo/V )
+    rA = r1.add_reaction('rA', k1*A)
+    rB = r1.add_reaction('rB', k2*B)
+    
+    SM = {
+        'A': [ -1,  0],
+        'B': [0.5, -1],
+        'C': [  0,  3]
+        }
+    
+    RE = r1.reactions_from_stoich(SM, add_odes=False)
+    RE['A'] += cA0*v0/V
+    r1.add_odes(RE)
+    
+    # Energy balances
+    T1 = Uc*(Tc - T) # heat transfer
+    T2 = cA0*v0*cpA*(T - TA0) # enthalpy from adding A
+    T3 = V*(hrxnA*rA - hrxnB*rB) # heat of reaction
+    Den = (cpA*A + cpB*B + cpC*C)*V + 3500.0 # heat capacities/density
+    
     r1.add_ode('T', (T1 + T2 + T3)/Den )
-    r1.add_ode('V', vo )
+    
+    # ODE for volume
+    r1.add_ode('V', v0 )
     
     # Simulation requires a time span
     r1.set_time(2.0)
     
+    # This is difficult to simulate because it is really stiff
     # Change some of the default settings
-    r1.settings.collocation.nfe = 40
+    r1.settings.collocation.nfe = 120
     r1.settings.collocation.ncp = 1
-    r1.settings.general.add_volume_terms = False
-    r1.settings.simulator.method = 'dae.collocation'
+    r1.settings.general.add_volume_terms = True
+    #r1.settings.simulator.method = 'dae.collocation'
 
     # Simulation
     r1.simulate()  
 
     # Create plots
     if with_plots:
-        r1.plot()
+        r1.plot('Z')
